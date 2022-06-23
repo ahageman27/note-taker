63527f055cc3b4b649981422a73203ef9f80dbcf
Alex Hageman <ahageman27@gmail.com>
2022-06-23 11:05:51 -0600

    deployed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const noteData = require("./db/db.json");
 
 const app = express();
 const PORT = 3001;
@@ -14,7 +15,33 @@ app.get("/", (req, res) => {
 });
 
 app.get("/notes", (req, res) => {
+  res.json(noteData);
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => res.json(noteData));
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note.`);
+  const {title, text} = req.body
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+
+  } else {
+    res.status(500).json("Request body must contain a title and a body.");
+  }
 });
 
 app.listen(PORT, () =>
